7f8427367d6d1ccb5e4fc6ccb6b94a7e63a91575
IvanSolovyov23 <solovevivan474@gmail.com>
2023-12-03 21:44:07 +0300

    Добавлены задания 6,9

---
+++ laboratory-task-2/Лабораторная работа 2 (6).cpp
@@ -0,0 +1,37 @@
+﻿// Написать программу, которая для заданного натурального числа выполняет разложение числа на простые множители
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n = 0;
+	while (n < 1)
+	{
+		cout << "Input natural n:";
+		cin >> n;
+	}
+	cout << n << " = ";
+	while (n > 1)
+	{
+		for (int i = 2; i <= n;)
+		{
+			if (n % i)
+			{
+				++i;
+			}
+			else
+			{
+				n /= i;
+				if (n != 1)
+				{
+					cout << i << " * ";
+				}
+				else
+				{
+					cout << i;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
---
+++ laboratory-task-2/Лабораторная работа 2 (9).cpp
@@ -0,0 +1,43 @@
+﻿// Написать программу, которая для заданного натурального числа N находит дружественное, не превышающие это число:
+#include <iostream>
+using namespace std;
+	int main ()
+	{
+		int n = 0;
+		while (n < 1)
+		{
+			cout << "Input in:";
+			cin >> n;
+		}
+		int temp1, temp2;
+		for (int i = 2; i <= n; ++i)
+		{
+			temp1 = i;
+			int sum1 = 1;
+			int sum2 = 1;
+			for (int j = 2; j < temp1; ++j)
+			{
+				if (!(temp1 % j))
+				{
+					sum1 += j;
+				}
+			}
+			temp2 = sum1;
+			for (int k = 2; k < temp2; ++k)
+			{
+				if (!(temp2 % k))
+				{
+					sum2 += k;
+				}
+			}
+			if (sum2 == temp1 && temp1 < temp2)
+			{
+				cout << temp1 << " " << "and" << " " << temp2 << endl;
+			}
+		}
+
+		return 0;
+    }
+
+	
+
