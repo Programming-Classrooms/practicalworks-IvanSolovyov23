1bbb9a9c70c4675ad817d32291b9cff20bdd6391
IvanSolovyov23 <146059275+IvanSolovyov23@users.noreply.github.com>
2024-05-06 09:57:45 +0300

    Update main.cpp
    Исправил кодировку

--- laboratory-task-13-1/src/main/main.cpp
+++ laboratory-task-13-1/src/main/main.cpp
@@ -1,9 +1,9 @@
-
 #include "../funcs/Fraction.hpp"
 
 
 
-int main() {
+int main() 
+{
 	try {
 		Fraction fraction1(3, 4);
 		Fraction fraction2(1, 2);
@@ -16,7 +16,7 @@ int main() {
         fraction2.print();
         std::cout << std::endl;
 
-        // Áèíàðíûå àëãåáðàè÷åñêèå îïåðàòîðû
+        // Бинарные алгебраические операторы
         std::cout << "Fraction 1 + Fraction 2: ";
         (fraction1 + fraction2).print();
         std::cout << std::endl;
@@ -33,11 +33,11 @@ int main() {
         (fraction1 / fraction2).print();
         std::cout << std::endl;
 
-        // Ïðîâåðêà ãåòòåðîâ
+        // Проверка геттеров
         std::cout << "Numerator of Fraction 1: " << fraction1.getNumerator() << std::endl;
         std::cout << "Denominator of Fraction 1: " << fraction1.getDenominator() << std::endl;
 
-        // Ïðîâåðêà ñåòòåðîâ
+        // Проверка сеттеров
         fraction1.setNumerator(5);
         fraction1.setDenominator(3);
         std::cout << "Modified Fraction 1: ";
@@ -46,55 +46,55 @@ int main() {
 
         int32_t number = 3;
 
-        // ×èñëî + äðîáü
+        // Число + дробь
         Fraction result_addition_number_fraction = number + fraction1;
         std::cout << "Number + Fraction: ";
         result_addition_number_fraction.print();
         std::cout << std::endl;
 
-        // ×èñëî - äðîáü
+        // Число - дробь
         Fraction result_subtraction_number_fraction = number - fraction1;
         std::cout << "Number - Fraction: ";
         result_subtraction_number_fraction.print();
         std::cout << std::endl;
 
-        // ×èñëî * äðîáü
+        // Число * дробь
         Fraction result_multiplication_number_fraction = number * fraction1;
         std::cout << "Number * Fraction: ";
         result_multiplication_number_fraction.print();
         std::cout << std::endl;
 
-        // ×èñëî / äðîáü
+        // Число / дробь
         Fraction result_division_number_fraction = number / fraction1;
         std::cout << "Number / Fraction: ";
         result_division_number_fraction.print();
         std::cout << std::endl;
 
-        // Äðîáü + ÷èñëî
+        // Дробь + число
         Fraction result_addition_fraction_number = fraction1 + number;
         std::cout << "Fraction + Number: ";
         result_addition_fraction_number.print();
         std::cout << std::endl;
 
-        // Äðîáü - ÷èñëî
+        // Дробь - число
         Fraction result_subtraction_fraction_number = fraction1 - number;
         std::cout << "Fraction - Number: ";
         result_subtraction_fraction_number.print();
         std::cout << std::endl;
 
-        // Äðîáü * ÷èñëî
+        // Дробь * число 
         Fraction result_multiplication_fraction_number = fraction1 * number;
         std::cout << "Fraction * Number: ";
         result_multiplication_fraction_number.print();
         std::cout << std::endl;
 
-        // Äðîáü / ÷èñëî
+        // Дробь / число
         Fraction result_division_fraction_number = fraction1 / number;
         std::cout << "Fraction / Number: ";
         result_division_fraction_number.print();
         std::cout << std::endl;
 
-        // Îïåðàöèè ñðàâíåíèÿ
+        // Операции сравнения
         std::cout << "Is Fraction 1 equal to Fraction 2? " << std::boolalpha << (fraction1 == fraction2) << std::endl;
         std::cout << "Is Fraction 1 not equal to Fraction 2? " << std::boolalpha << (fraction1 != fraction2) << std::endl;
         std::cout << "Is Fraction 1 less than Fraction 2? " << std::boolalpha << (fraction1 < fraction2) << std::endl;
@@ -102,11 +102,11 @@ int main() {
         std::cout << "Is Fraction 1 greater than Fraction 2? " << std::boolalpha << (fraction1 > fraction2) << std::endl;
         std::cout << "Is Fraction 1 greater than or equal to Fraction 2? " << std::boolalpha << (fraction1 >= fraction2) << std::endl;
 
-        // Ïðåîáðàçîâàíèå ê double
+        // Преобразование к double
         double decimalValue = static_cast<double>(fraction1);
         std::cout << "Fraction 1 as a double: " << decimalValue << std::endl;
 
-        // Ïðåîáðàçîâàíèå ê int32_t
+        // Преобразование к int32_t
         int32_t intValue = static_cast<int32_t>(fraction1);
         std::cout << "Fraction 1 as int32_t: " << intValue << std::endl;
 
