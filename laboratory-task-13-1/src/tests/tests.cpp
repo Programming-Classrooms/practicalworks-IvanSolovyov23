02861cf8484e60012592a4d587acea0982a61b56
IvanSolovyov23 <146059275+IvanSolovyov23@users.noreply.github.com>
2024-05-22 10:03:42 +0300

    Исправил кодировку

--- laboratory-task-13-1/src/tests/tests.cpp
+++ laboratory-task-13-1/src/tests/tests.cpp
@@ -1,4 +1,3 @@
-
 #include <stdexcept>
 #include <gtest/gtest.h>
 #include "../funcs/Fraction.hpp"
@@ -9,7 +8,6 @@ TEST(TestCaseName, TestName) {
   EXPECT_TRUE(true);
 }
 
-// Òåñò äëÿ îïåðàòîðà ïðèñâàèâàíèÿ
 TEST(FractionTest, AssignmentOperatorTest) {
     Fraction frac1(3, 4);
     Fraction frac2;
@@ -17,7 +15,6 @@ TEST(FractionTest, AssignmentOperatorTest) {
     EXPECT_EQ(frac1, frac2);
 }
 
-// Òåñò äëÿ îïåðàòîðà ñëîæåíèÿ
 TEST(FractionTest, AdditionOperatorTest) {
     Fraction frac1(1, 2);
     Fraction frac2(1, 3);
@@ -25,7 +22,6 @@ TEST(FractionTest, AdditionOperatorTest) {
     EXPECT_EQ(result, Fraction(5, 6));
 }
 
-// Òåñò äëÿ îïåðàòîðà âû÷èòàíèÿ
 TEST(FractionTest, SubtractionOperatorTest) {
     Fraction frac1(3, 4);
     Fraction frac2(1, 2);
@@ -49,28 +45,24 @@ TEST(FractionTest, DivisionOperatorTest) {
     EXPECT_EQ(result, Fraction(3, 2));
 }
 
-// Òåñò äëÿ êîíñòðóêòîðà ñ ïàðàìåòðàìè
 TEST(FractionTest, ConstructorTest) {
     Fraction frac(3, 4);
     EXPECT_EQ(frac.getNumerator(), 3);
     EXPECT_EQ(frac.getDenominator(), 4);
 }
 
-// Òåñò äëÿ ìåòîäîâ getNumerator è getDenominator
 TEST(FractionTest, GetNumeratorAndDenominatorTest) {
     Fraction frac(3, 4);
     EXPECT_EQ(frac.getNumerator(), 3);
     EXPECT_EQ(frac.getDenominator(), 4);
 }
 
-// Òåñò äëÿ ìåòîäà setNumerator
 TEST(FractionTest, SetNumeratorTest) {
     Fraction frac;
     frac.setNumerator(5);
     EXPECT_EQ(frac.getNumerator(), 5);
 }
 
-// Òåñò äëÿ îïåðàòîðà ñëîæåíèÿ ñ äðóãîé äðîáüþ
 TEST(FractionTest, AdditionOperatorWithFractionTest) {
     Fraction frac1(1, 2);
     Fraction frac2(1, 3);
@@ -78,7 +70,7 @@ TEST(FractionTest, AdditionOperatorWithFractionTest) {
     EXPECT_EQ(result, Fraction(5, 6));
 }
 
-// Òåñò äëÿ îïåðàòîðà âû÷èòàíèÿ ñ äðóãîé äðîáüþ
+// Òåñò äëÿ îïåðàòîðà âû÷èòàíèÿ ñ äðóãîé äðîáü
 TEST(FractionTest, SubtractionOperatorWithFractionTest) {
     Fraction frac1(3, 4);
     Fraction frac2(1, 2);
@@ -86,7 +78,6 @@ TEST(FractionTest, SubtractionOperatorWithFractionTest) {
     EXPECT_EQ(result, Fraction(1, 4));
 }
 
-// Òåñò äëÿ îïåðàòîðà óìíîæåíèÿ ñ äðóãîé äðîáüþ
 TEST(FractionTest, MultiplicationOperatorWithFractionTest) {
     Fraction frac1(3, 4);
     Fraction frac2(1, 2);
@@ -94,7 +85,6 @@ TEST(FractionTest, MultiplicationOperatorWithFractionTest) {
     EXPECT_EQ(result, Fraction(3, 8));
 }
 
-// Òåñò äëÿ îïåðàòîðà äåëåíèÿ ñ äðóãîé äðîáüþ
 TEST(FractionTest, DivisionOperatorWithFractionTest) {
     Fraction frac1(3, 4);
     Fraction frac2(1, 2);
