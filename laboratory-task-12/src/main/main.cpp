dcef82ca6d0418492b2b15d870fb907f9d4052c9
IvanSolovyov23 <solovevivan474@gmail.com>
2024-05-22 10:44:30 +0300

    Исправил кодировку

--- laboratory-task-12/src/main/main.cpp
+++ laboratory-task-12/src/main/main.cpp
@@ -4,22 +4,21 @@
 int main() 
 {
 	try {
-		// �������� � ����� ������
+		// Проверка конструктора
 		String str1("Kriptographia");
 		std::cout << "Main string: " << str1 << std::endl;
 
-		// ����������� �����������
 		String str2 = str1;
 		std::cout << "Copy main string: " << str2 << std::endl;
 
-		// �������� �������� � �������
+		// Проверка Геттеров и Сеттеров
 		std::cout << "Length of main string: " << str2.getLength() << std::endl; 
 		std::cout << "Data of main string: " << str2.getData() << std::endl; 
 
 		str1.setData("KrEptographia"); 
 		std::cout << "Main string after setting data: " << str1 << std::endl;
 
-		// �������� ���������� 
+        // Проверка алгебраических операторов
 		String str3(" MatImatica");
 		String str4 = str1 + str3; 
 		std::cout << "String after concatenation: " << str4 << std::endl; 
@@ -27,10 +26,10 @@ int main()
 		String str5 = str1 - str3;
 		std::cout << "String after substraction: " << str5 << std::endl;
 
-		str3 += " and Coding on C++"; // ����������
+		str3 += " and Coding on C++"; 
 		std::cout << "String after addition: " << str3 << std::endl; 
-
-		// ��������� ��������� 
+        
+		// Проверка операторов сравнения
 		String str6("VF");
 		String str7("ANB");
 		std::cout << "Is ANB equal to VF? " << std::boolalpha << (str6 == str7) << std::endl;
